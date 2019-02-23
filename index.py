d089bf3e4bb5a384c5f1e63d1cc2949356861e34
Cherub <apoorvkansalak@gmail.com>
2019-02-23 06:59:20 +0530

    fixed a bug while downloading chromedrive

--- index.py
+++ index.py
@@ -1,9 +1,30 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
-from PIL import Image
-from os import rename
+import time
+import sys
+import os
+import zipfile
+from urllib.request import urlretrieve
+try:
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.common.keys import Keys
+    from bs4 import BeautifulSoup
+    import lxml
+    from PIL import Image
+except ImportError:
+    import pip
+    try:
+        print("Installing Requirements for program.")
+        pip.main(['install', 'selenium'])
+        pip.main(['install', 'beautifulsoup4'])
+        pip.main(['install', 'lxml'])
+        pip.main(['install', 'Pillow'])
+        from selenium import webdriver
+        from selenium.webdriver.chrome.options import Options
+        from selenium.webdriver.common.keys import Keys
+        from bs4 import BeautifulSoup
+        from PIL import Image
+    except SystemExit as e:
+        print("Couldn't install the requirements. Please install selenium and beautifulsoup4 modules manually.")
 
 
 class Subject:
@@ -19,7 +40,7 @@ class Subject:
         self.class_left = self.__calc_skip_class()
 
     def __str__(self):
-        return self.code+" "+self.name + " " + str(self.class_left)
+        return self.code+" "+self.name + "\nClasses You can skip: " + str(self.class_left)
 
     def __calc_skip_class(self):
         return int((4*self.present - 3*self.total)/3)
@@ -30,7 +51,7 @@ def get_captcha(driver,element):
     :param driver: Takes the selenium object
     :return: The captcha as entered by user
     """
-    path = 'files/captchas/captcha.png'
+    path = 'Files/captchas/captcha.png'
     location = element.location
     size = element.size
     driver.save_screenshot(path)
@@ -40,29 +61,82 @@ def get_captcha(driver,element):
     right = location['x'] + size['width']
     bottom = location['y'] + size['height']
     image = image.crop((left, top, right, bottom))
-    # image.show();
+    image.show();
     captcha = input("Please enter the captcha code as shown\n")
-    image.save('files/captchas/captcha.png', 'png')
+    image.save('Files/captchas/captcha.png', 'png')
     return captcha
 
 
+def reporthook(blocknum, blocksize, totalsize):
+    """
+    function copied from https://stackoverflow.com/questions/13881092/download-progressbar-for-python-3
+    """
+    global start_time
+    if blocknum==0:
+        start_time = time.time()
+        return
+    if totalsize > 0:
+        duration = time.time() - start_time
+        readsofar = blocknum * blocksize
+        if duration == 0:
+            duration+=0.001
+        speed = int(readsofar / (1024 * duration))
+        percent = readsofar * 1e2 / totalsize
+        s = '\rPercentage : %5.1f%% (%5.2f MB out of %5.2f MB, Download Speed %d KB/s, %d seconds passed )' % (
+            percent, readsofar / 1048576, totalsize / 1048576, speed, duration)
+        sys.stderr.write(s)
+        if readsofar >= totalsize:  # near the end
+            sys.stderr.write("\n")
+    else:  # total size is unknown
+        sys.stderr.write("read %d\n" % (readsofar,))
+
+
+def download_file(url, filename, folder):
+    print("Downloading file : ", filename)
+    urlretrieve(url, os.path.join(folder, filename), reporthook)
+    print("Download Complete")
+    return True
+
+
+reg_num = input("Enter your register number\n")
+password = input("Enter your FFCS password\n")
 STUDENT_URL = "https://academicscc.vit.ac.in/student/stud_login.asp"
 ATTENDACE_URL = r'https://academicscc.vit.ac.in/student/attn_report.asp?sem=WS&fmdt=03-Dec-2018&todt=23-Feb-2019'
-# todo add function to dwnld the chrome driver
-chrome_driver = r'files/chromedriver.exe'
-driver = webdriver.Chrome(chrome_driver)
+
+
+# -------------------------------INITIALISE CHROME DRIVER--------------------------------
+options = Options()
+options.add_argument('log-level=3')
+options.headless = True
+chrome_driver = r'Files/chromedriver.exe'
+
+if not os.path.exists(chrome_driver):   # Checking and downloading chrome driver
+    print("Chrome driver does not exist. Downloading it and saving in {Files} folder")
+    # TODO Download chrome driver for platform specific
+    # check using sys.platform, win32, linux, darwin
+    os.mkdir('Files')
+    download_file("https://chromedriver.storage.googleapis.com/2.44/chromedriver_win32.zip", "chromedriver.zip",
+                  "Files")
+    print("Extracting components")
+    with zipfile.ZipFile(os.path.join("Files", "chromedriver.zip"), "r") as zip_ref:
+        zip_ref.extractall("./Files")
+    print("Complete.")
+
+
+driver = webdriver.Chrome(chrome_driver, options=options)
 driver.get(STUDENT_URL)
 
 captcha_img = driver.find_element_by_xpath('//*[@id="imgCaptcha"]')
 captcha = ""
 try:
     captcha = get_captcha(driver, captcha_img).upper()
-    rename('files/captchas/captcha.png', 'files/captchas/'+captcha+'.png')
+    os.rename('Files/captchas/captcha.png', 'Files/captchas/'+captcha+'.png')
 except Exception as e:
     print(e)
+
 data = {
-    'regno': '17bec1162',
-    'passwd': 'tempPass123@',
+    'regno': reg_num,
+    'passwd': password,
     'vrfcd': captcha,
 }
 
